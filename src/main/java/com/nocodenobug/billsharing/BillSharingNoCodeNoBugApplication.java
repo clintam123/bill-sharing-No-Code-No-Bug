ff5504d1f58945f5a92e131573d48703329f09d7
clintam123 <nguyenhanh1074@gmail.com>
2022-06-29 15:54:12 +0700

    first commit

---
+++ src/main/java/com/nocodenobug/billsharing/BillSharingNoCodeNoBugApplication.java
@@ -0,0 +1,13 @@
+package com.nocodenobug.billsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSharingNoCodeNoBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillSharingNoCodeNoBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nocodenobug/billsharing/BillSharingNoCodeNoBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocodenobug.billsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSharingNoCodeNoBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
