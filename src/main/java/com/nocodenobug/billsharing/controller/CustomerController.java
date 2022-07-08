109cabd3ed1e3098338587ed00df29bc26c7605b
hoaquangsy <77135777+hoaquangsy@users.noreply.github.com>
2022-07-08 22:03:30 +0700

    addSwaggerCustomerAndVendor

--- src/main/java/com/nocodenobug/billsharing/controller/CustomerController.java
+++ src/main/java/com/nocodenobug/billsharing/controller/CustomerController.java
@@ -5,12 +5,17 @@ import com.nocodenobug.billsharing.response.Pagination;
 import com.nocodenobug.billsharing.response.SamplePagingResponse;
 import com.nocodenobug.billsharing.response.SampleResponse;
 import com.nocodenobug.billsharing.service.customerservice.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(
+        description = "Customer controller",
+        name = "Các api về Customer (dành cho admin)"
+)
 @RestController
 @RequestMapping("/api/v1.0/admin/customer")
 public class CustomerController {
@@ -26,7 +31,9 @@ public class CustomerController {
     private GetCustomerByIdService getCustomerByIdService;
     @Autowired
     private SearchCustomerBySdtService searchCustomerBySdtService;
-
+    @Operation(summary = "Lấy tất cả Customer", description =
+            "page: trang hiện tại (bắt đầu từ 0), page_size: số record trong trang hiện tại,"
+    )
     @GetMapping("/get-all")
     public ResponseEntity<?> getAll(
             @RequestParam(value = "page") int page,
@@ -48,7 +55,7 @@ public class CustomerController {
                 .build());
 
     }
-
+    @Operation(summary = "Lấy customer theo Id", description = "Lấy customer theo Id")
     @GetMapping("/get-customer/{id}")
     public ResponseEntity<?> getAll(
             @PathVariable Long id
@@ -61,7 +68,7 @@ public class CustomerController {
                 .build()
         );
     }
-
+    @Operation(summary = "Tạo customer", description = "Tạo customer")
     @PostMapping("/add-customer")
     public ResponseEntity<?> addCustomer(@Validated @RequestBody CustomerDto customerDto){
        return ResponseEntity.ok(SampleResponse.builder()
@@ -71,6 +78,7 @@ public class CustomerController {
                .build()
        );
     }
+    @Operation(summary = "Update customer", description = "Update customer")
     @PutMapping("/update-customer/{id}")
     public ResponseEntity<?> updateCustomer(@PathVariable Long id,@Validated @RequestBody CustomerDto customerDto){
         return ResponseEntity.ok(SampleResponse.builder()
@@ -80,6 +88,7 @@ public class CustomerController {
                 .build()
         );
     }
+    @Operation(summary = "Xóa customer", description = "Xóa customer")
     @DeleteMapping("/delete-customer/{id}")
     public ResponseEntity<?> deleteCustomer(
             @PathVariable Long id
@@ -93,7 +102,7 @@ public class CustomerController {
                 .build()
         );
     }
-
+    @Operation(summary = "Tìm kiếm customer theo SDT", description = "Tìm kiếm customer theo SDT")
     @GetMapping("/search-customerByPhone/{phone}")
     public ResponseEntity<?> searchCustomerByPhone(
             @PathVariable String phone
